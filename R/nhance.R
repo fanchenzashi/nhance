0c957972f3d752ad318bf31c021c86398424f04d
fanchenzashi <88575317+fanchenzashi@users.noreply.github.com>
2021-08-17 15:41:48 +0800

    Add files via upload

--- R/nhance.R
+++ R/nhance.R
@@ -11,7 +11,9 @@
 #' @examples nhance('*.xpt','name.csv')
 nhance <- function(file,output){
   i <- haven::read_xpt(file)
-  seqn <- data.frame(SEQN=1:max(a$SEQN))
+  seqn <- data.frame(SEQN=1:max(i$SEQN))
   o <- dplyr::full_join(i,seqn,by='SEQN')
   write.csv(o,output)
 }
+
+
